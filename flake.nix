154742b6360d5d03d805803ef25e0cd0ae29d1b5
Mirza Hilmi <dev@mrz.my.id>
2024-06-28 18:12:38 +0700

    feat: setup dev environment via flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Avanzu Backend dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          air
+          delve
+	  go
+          go-migrate 
+          golangci-lint
+          gopls
+        ];
+      };
+    });
+}
